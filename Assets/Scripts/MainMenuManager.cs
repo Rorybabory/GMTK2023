f523af3d178c7b7701841642f1710b50fac993ed
Brevillo <oliver.beebe@digipen.edu>
2023-07-08 22:19:45 -0700

    settings and menu transitions complete

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour {
 
+    [SerializeField] private SmartCurve menuTransition;
+    [SerializeField] private RectTransform menuPosition;
+
     private enum State { main, settings, levelSelect }
 
+    private Dictionary<State, Vector2> menuPositions = new() {
+        { State.main, Vector2.zero },
+        { State.settings, new(1600, 0) },
+        { State.levelSelect, new(-1600, 0) },
+    };
+
     private State state = State.main;
+    private Vector2 position, start, end;
+
+    private void ChangeState(State newState) {
+        start = position;
+        end = menuPositions[newState];
+        state = newState;
+        menuTransition.Start();
+    }
+
+    private void Update() {
+        position = Vector2.LerpUnclamped(start, end, menuTransition.Evaluate());
+        menuPosition.localPosition = position;
+    }
 
     public void StartGame() {
-        state = State.levelSelect;
+        ChangeState(State.levelSelect);
     }
 
     public void Settings() {
-        state = State.settings;
+        ChangeState(State.settings);
     }
 
     public void Back() {
-        state = State.main;
+        ChangeState(State.main);
     }
+
+    public void MusicVolume(float volume) => Audio.MusicVolume = volume;
+    public void SoundVolume(float volume) => Audio.SoundVolume = volume;
 }
